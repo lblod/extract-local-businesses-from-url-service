864109422abaa6f9139d5fb050052cb1c51a576b
Aad Versteden <madnificent@gmail.com>
2020-05-18 18:05:52 +0200

    Sign of life
    First sign of life.  The service returns triples.

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-// see https://github.com/mu-semtech/mu-javascript-template for more info
-
 import { app, query, errorHandler } from 'mu';
+import rdfDereferencer from "rdf-dereference";
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
+app.get('/', async function( req, res ) {
+  const URL = req.query.url;
+  try {
+    let quadArray = [];
+    const response = await rdfDereferencer.dereference( URL, { headers: { "Accept": "text/html" } } );
+
+    await new Promise((success,failure) => {
+      response
+        .quads
+        .on("data", (quad) => quadArray.push(quad) )
+        .on("error", (message) => {
+          console.warn(`Failed to pass through url ${URL}`);
+          failure(message);
+        })
+        .on("end", () => {
+          success( quadArray );
+        });
+    });
+    res
+      .status(200)
+      .send(JSON.stringify( quadArray ));
+
+  } catch (e) {
+    console.error(`Error occurred calculating response for url ${URL}`);
+    res
+      .status(500)
+      .send(JSON.stringify({ status: 500, message: "Something went wrong while getting the URL dereferenced." }));
+  }
 } );
 
 app.use(errorHandler);
